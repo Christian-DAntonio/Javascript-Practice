8f2887ce2bae04fa0c74ee89b4f068a8c2c520d1
Christian-DAntonio <christian.dantonio@stclairconnect.ca>
2025-01-16 10:54:42 -0500

    Update app.js

--- Grocerry/app.js
+++ Grocerry/app.js
@@ -13,8 +13,7 @@ let editID = "";
 form.addEventListener("submit", addItem);
 clearBtn.addEventListener('click', clearItems);
 
-const deleteBtn = document.querySelector('.delete-btn');
-console.log(deleteBtn);
+
 
 function addItem(e) {
  e.preventDefault();
@@ -69,6 +68,20 @@ function displayAlert(text,action){
 }
 
 function clearItems(){
+ const items = document.querySelectorAll('.grocery-item');
+
+ if(items.length > 0){
+  items.forEach(function(item) {
+    list.removeChild(item);
+  })
+ }
+ container.classList.remove("show-container");
+ displayAlert("empty list", "danger");
+ setBackToDefault();
+ // localStorage.removeItem('list');
+}
+
+function clearItems(){
   const items = document.querySelectorAll('.grocery-item');
 
   if(items.length >  0){
@@ -82,11 +95,25 @@ function clearItems(){
 }
 
 function deleteItem(){
-  console.log('item deleted');
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if(list.children.length === 0){
+    container.classList.remove("show-container");
+  }
+  displayAlert('item removed', 'danger');
+  setBackToDefault();
 }
 
 function editItem(){
-  console.log('edit item');
+  const element = e.currentTarget.parentElement.parentElement;
+  editElement = e.currentTarget.parentElement.
+  previousElementSibling;
+
+  grocery.value = editElement.innerHTML;
+  editFlag = true;
+  editID = element.dataset.id;
+  submitBtn.textContent = "edit";
 }
 
 function setBackToDefault(){
@@ -100,3 +127,4 @@ function setBackToDefault(){
 function addToLocalStorage(id,value){
   console.log("added to local storgae");
 }
+
